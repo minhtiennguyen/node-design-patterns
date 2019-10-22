1b3d2d308f1090bb3d2bb53a48fa28d673301cc6
Minh Tien Nguyen <tien.minh@hubcba.com>
2019-10-22 14:27:20 +0700

    proplem for singleton

---
+++ index.js
@@ -0,0 +1,19 @@
+var Logger = require('./singleton/Logger');
+var Shopper = require('./singleton/Shopper');
+var Store = require('./singleton/Store');
+
+var logger = new Logger();
+
+logger.log('Starting config...');
+
+var alex = new Shopper('alex', 500);
+var ski_shop = new Store('Steep and Deep Supplies', [
+  { item:'Downhill Skis', qty: 5, price: 750 },
+  { item:'Knit Hat', qty: 20, price: 5 },
+]);
+
+logger.log('Finished config...');
+
+console.log(`${logger.count} logs total`);
+
+logger.logs.map(log => console.log(`${log.message}`));
