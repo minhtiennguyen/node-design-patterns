bf52caeab2a3f44294e4d3ca771867246cccbab1
Minh Tien Nguyen <tien.minh@hubcba.com>
2019-12-10 18:33:14 +1100

    finish Composite

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 var CatalogItem = require('./CatalogItem');
+var CatalogGroup = require('./CatalogGroup');
 
-var boots = new CatalogItem("Leather Boots", 79.99);
-var sneakers = new CatalogItem("Kicks", 39.99);
-var flipFlops = new CatalogItem("California work boots", 19.99);
+var boots = new CatalogItem('Leather Boots', 79.99);
+var sneakers = new CatalogItem('Kicks', 39.99);
+var flipFlops = new CatalogItem('California work boots', 19.99);
 
-console.log( 'boots total: ', `$${boots.total}` );
+var group_shoes = new CatalogGroup('Shoes and such', [boots, sneakers, flipFlops]);
 
-boots.print();
-sneakers.print();
+var group_food = new CatalogGroup('Food for while you try on clothes', [
+  new CatalogItem('Potato Chips', 1.99),
+  new CatalogItem('Hamburger', 3.99)
+]);
+
+var keychain = new CatalogItem('Keychain', 2.99);
+
+var catalog = new CatalogGroup('Clothes and Food', [group_shoes, group_food, keychain]);
+
+console.log('catalof total: ', `$${catalog.total}`);
+catalog.print();
+
+// console.log('boots total: ', `$${boots.total}`);
+// console.log('shoes total: ', `$${group_shoes.total}`);
+
+// boots.print();
+// sneakers.print();
+// group_shoes.print();
