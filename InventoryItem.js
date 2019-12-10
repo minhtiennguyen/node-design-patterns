83d78d087ed6c4ac1f256980de25cf38a5808035
Minh Tien Nguyen <tien.minh@hubcba.com>
2019-12-10 18:55:39 +1100

    finish Decorator

--- InventoryItem.js
+++ InventoryItem.js
@@ -1,14 +1,31 @@
 class InventoryItem {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
 
-    constructor(name, price) {
-        this.name = name
-        this.price = price
-    }
-
-    print() {
-        console.log(`${item.name} costs ${item.price}`)
-    }
-
+  print() {
+    console.log(`${item.name} costs ${item.price}`);
+  }
 }
 
-module.exports = InventoryItem;
+class GoldInventoryItem {
+  constructor(baseItem) {
+    this.name = `Golden ${baseItem.name}`;
+    this.price = 1000 + baseItem.price;
+  }
+}
+
+class DiamondInventoryItem {
+  constructor(baseItem) {
+    this.name = `Diamond ${baseItem.name}`;
+    this.price = 1000 + baseItem.price;
+    this.cutsGlass = true;
+  }
+
+  print() {
+      console.log(`${this.name} cost alot of money...`);
+  }
+}
+
+module.exports = { InventoryItem, GoldInventoryItem, DiamondInventoryItem };
--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 var Shopper = require('./Shopper');
-var InventoryItem = require('./InventoryItem');
+var {
+  InventoryItem,
+  GoldInventoryItem,
+  DiamondInventoryItem
+} = require('./InventoryItem');
 
-var alex = new Shopper('Alex', 100);
+var alex = new Shopper('Alex', 4000);
 
 var walkman = new InventoryItem("Walkman", 29.99);
 var necklace = new InventoryItem("Necklace", 9.99);
 
-alex.purchase(necklace);
-alex.purchase(walkman);
+var gold_necklace = new GoldInventoryItem(necklace);
+var diamond_gold_necklace = new DiamondInventoryItem(gold_necklace);
+var gold_walkman = new GoldInventoryItem(walkman);
+
+
+alex.purchase(diamond_gold_necklace);
+alex.purchase(gold_walkman);
 
 alex.printStatus();
+diamond_gold_necklace.print();
