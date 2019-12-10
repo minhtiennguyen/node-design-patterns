66612ec3abe6434ced9fcad7fec11811a1954ea4
Minh Tien Nguyen <tien.minh@hubcba.com>
2019-12-10 19:26:14 +1100

    finish chain of reposibility

--- Store.js
+++ Store.js
@@ -1,12 +1,25 @@
+var Storage = require('./Storage');
+
 class Store {
   constructor(name, inventory = []) {
     this.name = name;
-    this.inventory = inventory;
+    // this.inventory = inventory;
+    var floor = new Storage('store floor', inventory.floor);
+    var backRoom = new Storage('store backroom', inventory.backRoom);
+    var localStore = new Storage('nearby store', inventory.localStore, 1);
+    var warehouse = new Storage('warehouse', inventory.warehouse, 5);
+
+    floor.setNext(backRoom);
+    backRoom.setNext(localStore);
+    localStore.setNext(warehouse);
+
+    this.storage = floor;
   }
 
   find(itemName) {
-    var index = this.inventory.map(item => item.name).indexOf(itemName);
-    return this.inventory[index];
+    // var index = this.inventory.map(item => item.name).indexOf(itemName);
+    // return this.inventory[index];
+    return this.storage.find(itemName);
   }
 }
 
