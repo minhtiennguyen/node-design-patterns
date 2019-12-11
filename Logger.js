721ac4d8ea7514f146ab9216e6ded3efc14d0d44
Minh Tien Nguyen <tien.minh@hubcba.com>
2019-12-11 14:31:37 +1100

    finish strategy

--- Logger.js
+++ Logger.js
@@ -1,19 +1,25 @@
+var LogStrategy = require('./LogStrategy');
+var config = require('./config');
 class Logger {
+  constructor(strategy='toConsole') {
+    this.logs = [];
+    this.strategy = LogStrategy[strategy];
+  }
 
-    constructor() {
-        this.logs = [];
-    }
+  get count() {
+    return this.logs.length;
+  }
 
-    get count() {
-        return this.logs.length
-    }
+  changeStrategy(newStrategy) {
+      this.strategy = LogStrategy[newStrategy];
+  }
 
-    log(message) {
-        const timestamp = new Date().toISOString()
-        this.logs.push({ message, timestamp })
-        console.log(`${timestamp} - ${message}`)
-    }
-
+  log(message) {
+    const timestamp = new Date().toISOString();
+    this.logs.push({ message, timestamp });
+    // console.log(`${timestamp} - ${message}`);
+    this.strategy(timestamp, message);
+  }
 }
 
-module.exports = new Logger();
+module.exports = new Logger(config.logs.strategy);
