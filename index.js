152ddbfab88b1fa7620e45dcfce190f2d993ceec
Minh Tien Nguyen <tien.minh@hubcba.com>
2019-12-11 14:02:27 +1100

    init

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
-var logger = require('./singleton/Logger');
-var Shopper = require('./Shopper');
-var Store = require('./Store');
+var logger = require('./Logger');
 
-// var logger = new Logger();
-
-logger.log('Starting config...');
-
-var alex = new Shopper('alex', 500);
-var ski_shop = new Store('Steep and Deep Supplies', [
-  { item:'Downhill Skis', qty: 5, price: 750 },
-  { item:'Knit Hat', qty: 20, price: 5 },
-]);
-
-logger.log('Finished config...');
-
-console.log(`${logger.count} logs total`);
-
-logger.logs.map(log => console.log(`${log.message}`));
+logger.log('Hello World');
+logger.log('Hi World');
+logger.log('Yo World');
